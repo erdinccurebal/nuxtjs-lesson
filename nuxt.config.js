d5fd3298dd8fef729032161a11bb3d403c514862
Erdinç Cürebal <erdinccurebal@hotmail.com>
2021-01-03 23:28:10 +0300

    first init

---
+++ nuxt.config.js
@@ -0,0 +1,29 @@
+export default {
+    // ! Global head ayarları.
+    head: {
+        title: 'NuxtJS Lesson',
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            }
+        ],
+    },
+
+    // ! Global css dosyalarımız.
+    css: [],
+
+    // ! Dışardan ekleyebileceğimiz js yapıları.
+    plugins: [],
+
+    // ! Otomatik component angılama kodu.
+    components: true,
+
+    // ! NuxtJS ait modüllerimiz.
+    modules: [],
+
+    // ! NuxtJS ayarları
+    build: {}
+}
